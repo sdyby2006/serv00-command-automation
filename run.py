8ad417ed1f2fcc243352af94153345c41b933ad9
Sakura丶舞随樱雨 <sdyby2006@163.com>
2024-11-12 12:49:52 +0800

    Update run.py

--- run.py
+++ run.py
@@ -4,13 +4,15 @@ import requests
 import json
 from datetime import datetime, timezone, timedelta
 
-def ssh_multiple_connections(hosts_info, command):
+def ssh_multiple_connections(hosts_info):
     users = []
     hostnames = []
+    commands = []
     for host_info in hosts_info:
         hostname = host_info['hostname']
         username = host_info['username']
         password = host_info['password']
+        command = host_info['command']
         try:
             ssh = paramiko.SSHClient()
             ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
@@ -19,20 +21,20 @@ def ssh_multiple_connections(hosts_info, command):
             user = stdout.read().decode().strip()
             users.append(user)
             hostnames.append(hostname)
+            commands.append(command)
             ssh.close()
         except Exception as e:
             print(f"用户：{username}，连接 {hostname} 时出错: {str(e)}")
-    return users, hostnames
+    return users, hostnames, commands
 
 ssh_info_str = os.getenv('SSH_INFO', '[]')
 hosts_info = json.loads(ssh_info_str)
 
-command = 'whoami'
-user_list, hostname_list = ssh_multiple_connections(hosts_info, command)
+user_list, hostname_list, command_list = ssh_multiple_connections(hosts_info)
 user_num = len(user_list)
 content = "SSH服务器登录信息：\n"
-for user, hostname in zip(user_list, hostname_list):
-    content += f"用户名：{user}，服务器：{hostname}\n"
+for user, hostname, command in zip(user_list, hostname_list, command_list):
+    content += f"用户名：{user}，服务器：{hostname}，命令：{command}\n"
 beijing_timezone = timezone(timedelta(hours=8))
 time = datetime.now(beijing_timezone).strftime('%Y-%m-%d %H:%M:%S')
 menu = requests.get('https://api.zzzwb.com/v1?get=tg').json()
